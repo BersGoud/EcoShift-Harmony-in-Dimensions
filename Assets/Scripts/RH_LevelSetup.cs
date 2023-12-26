344e8c2ae2904c21278f770737be9359b456b4e5
Roan Heylen <roanheylen@hotmail.com>
2023-12-26 16:47:11 +0100

    Setup for my level + Event fix for when camera changes.

---
+++ Assets/Scripts/RH_LevelSetup.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DimensionalShift.isDimensionalShiftEnabled = true;
+        DimensionalShift.OnCameraChanged += DimensionalShift_OnCameraChanged;
+    }
+
+    private void DimensionalShift_OnCameraChanged(bool cameraIs3D)
+    {
+        GameObject[] remove = GameObject.FindGameObjectsWithTag("RemoveIn2D");
+        if (!cameraIs3D)
+        {
+            foreach (GameObject obj in remove)
+            {
+                obj.SetActive(false);
+            }
+        } else
+        {
+            foreach (GameObject obj in remove)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
