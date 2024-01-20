b5eeded2e96c8975b9f88cfc48d4fc1b6ad6df7e
jVaerewijck <vaerewijckjarn@gmail.com>
2024-01-20 18:30:37 +0100

    bridgemaker+CableGameManager

---
+++ Assets/Scripts/Cable connect/CableGamemanager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableGamemanager
+{
+    private static CableGamemanager instance;
+
+    public static CableGamemanager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new CableGamemanager();
+            }
+            return instance;
+        }
+    }
+    // Game progress variable
+    private bool miniGameCompleted = false;
+    private bool minibool = false;
+
+    // Other game-related variables and methods...
+
+    public void SetMiniGameCompleted()
+    {
+        // Set the game progress variable to true when the mini-game is completed
+        miniGameCompleted = true;
+        Debug.Log(miniGameCompleted);
+        //SceneManager.LoadScene(miniGameSceneName);
+
+        // Add any other logic you want to execute when the mini-game is completed
+    }
+
+    public bool IsMiniGameCompleted()
+    {
+        // Check the game progress variable to determine if the mini-game is completed
+        minibool = miniGameCompleted;
+        miniGameCompleted = false
+        return minibool;
+    }
+}
