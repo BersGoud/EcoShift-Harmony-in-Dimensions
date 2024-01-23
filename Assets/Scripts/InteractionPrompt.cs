287c725d4394b4f851b34e1cb6bd3fe04314f366
Roan Heylen <roanheylen@hotmail.com>
2024-01-23 19:11:59 +0100

    That didn't work hopefully this does.

--- Assets/Scripts/InteractionPrompt.cs
+++ Assets/Scripts/InteractionPrompt.cs
@@ -49,6 +49,11 @@ public class InteractionPrompt : MonoBehaviour
                     }
                 }
             }
+            else
+            {
+                InteractionCanvas.SetActive(false);
+                isPressedTextShown = false;
+            }
         }
         else
         {
