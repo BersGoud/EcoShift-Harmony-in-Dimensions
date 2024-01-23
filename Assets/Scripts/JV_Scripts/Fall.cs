0e505ff6b6564c9e50adba3f88845156b6e5295b
jVaerewijck <vaerewijckjarn@gmail.com>
2024-01-23 17:21:36 +0100

    bug fixes 2

--- Assets/Scripts/JV_Scripts/Fall.cs
+++ Assets/Scripts/JV_Scripts/Fall.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class LevelEnd : MonoBehaviour
+public class Fall : MonoBehaviour
 {
     public string sceneToLoad = "Level_JV_2"; 
-
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene(sceneToLoad);
