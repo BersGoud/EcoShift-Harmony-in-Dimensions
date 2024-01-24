7c02f9bc8c3d7942e3bf791d59e50b3e5079cb57
Michael Kaethoven <michael.kaethoven@gmail.com>
2024-01-24 12:15:25 +0100

    Updating the design of my level

---
+++ Assets/Scripts/MK_Scripts/Level_Setup_MK.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_Setup_MK : MonoBehaviour
+{
+
+    private GameObject _player;
+   
+
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        DimensionalShift.isDimensionalShiftEnabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
